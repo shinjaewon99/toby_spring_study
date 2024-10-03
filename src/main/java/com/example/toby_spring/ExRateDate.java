fd55eebde22b32783b60edacbfc86781a4f35b97
shinjaewon99 <tlswodnjs99@naver.com>
2024-10-03 10:45:21 +0900

    feat : 환율 계산 서비스 구현

---
+++ src/main/java/com/example/toby_spring/ExRateDate.java
@@ -0,0 +1,12 @@
+package com.example.toby_spring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+// json 타입의 필드를 모두 사용하지 않을경우, 필드 에러가 발생하므로
+// 필요한 필드만 사용하기 위해서는 아래의 어노테이션 사용
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateDate(String result, Map<String, BigDecimal> rates) {
+}
