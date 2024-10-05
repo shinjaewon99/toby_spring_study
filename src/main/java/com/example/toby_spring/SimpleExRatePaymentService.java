8c40e3a03dbcd7d20f3c30c7f3de3bbf7064aa5d
shinjaewon99 <tlswodnjs99@naver.com>
2024-10-05 20:02:14 +0900

    feat : 상속을 통한 확장

---
+++ src/main/java/com/example/toby_spring/SimpleExRatePaymentService.java
@@ -0,0 +1,13 @@
+package com.example.toby_spring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class SimpleExRatePaymentService extends PaymentService{
+    @Override
+    BigDecimal getExRate(final String currency) throws IOException {
+        if(currency.equals("USD")) return BigDecimal.valueOf(1000);
+
+        throw new IllegalArgumentException("지원되지 않는 통화 입니다.");
+    }
+}
